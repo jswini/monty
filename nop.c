854a8471a6716bd3a8ec5392b81977fe901cd087
ttkeele <3264@holbertonschool.com>
2021-09-14 18:00:28 +0000

    task 5 nop

--- nop.c
+++ nop.c
@@ -1,5 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: A pointer to the top mode node of a stack_t linked list
+ * @line_number: current working line number
+ */
+
+
 void  nop(stack_t **stack, unsigned int line_number)
 {
-	void *stack;
-	void line_number;
+	(void)*stack;
+	(void) line_number;
 }
